f77ba516a1c112c86c622e560b080e5477c51baa
Eren <erendore38k@gmail.com>
2023-11-21 21:46:07 +0300

    What the method is was demonstrated in a practical way.

---
+++ Metotlar/MetotNedir/Program.cs
@@ -0,0 +1,44 @@
+﻿
+/*
+ * METOD NEDİR?
+ *Yazılımda gerçekleştirdiğimiz,fiili olarak gerçekleştirdiğimiz tümm operasyonlar bir metotdur.Metotlara fonksiyonda diyebiliriz.
+ *Metot prosedürel programlamanın temel elemanıdır.Bir iş /fiiliyat/operasyon yapan en küçük program parçacıklarıdır.
+ *Metot işlem yapamanı sağlayan bir program parçacığıdır.
+ *Metorlar sayesinde kodlarımızı yazabiliyoruz,algoritmalarımızı oluşturabiliyoruz.
+ *Algoritma,Operasyon-Fiiliyat yapmamamızı sağlayan method.
+ *Esasında mimarisel tasarım dışında;özel algoritmalar programlar,program parçacıkları yazarken bir yazılımcının kesinlikle
+ *metoda ihtiyacı vardır. 
+ *İleri seviyede property denen yapılanmanın içerisinde de herhangi bir algoritma yapılabileceğini göreceğiz. İşte bu 
+ *propert'nin de özünde yani compiler seviyesinde bir metot olduğu gerçeğindendir. Aynı şekilde şimdiye kadar içerisinde
+ *prosedürel programlamaya dair öğrendiğimiz şeyleri denediğimiz main yapısının da bir metot olduğunu bilmekteyiz.
+ */
+
+
+/* 
+ *İşlevsel Açıdan Metot Bize Ne Kazandırır?
+ *Metod kodun içerisinde ki tekrarları kaldırmamıza yaramaktadır.
+ * Not:Yazılmış olan bir kodu tekrar çağırmakla,tekrar yazmak arasında fark vardır!
+ * Not:Metotlar içerisine yazılan kodun,tekrarlı bir şekilde kullanılmasını sağlarlar.Ve bunu yaparken kaynak kodun tekrar
+ * etmesinide engellemiş olurlar.Böylece ideal kodlamaya erişmiş oluruz.
+ */
+
+using System;
+
+namespace metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayilar = new int[10];
+            for (int i = 0; i < sayilar.Length; i++)
+            {
+                sayilar[i] = new Random().Next();
+            }
+            foreach (var sayi in sayilar)
+            {
+                Console.WriteLine(sayi);
+            }
+        }
+    }
+}
---
+++ Metotlar/README.md
@@ -0,0 +1,20 @@
+#### My-CSharp-Studies
+
+### EN
+* The aim here is to give information about the methods.
+* The examples are adjusted to the simple level.
+* You can contact me via e-mail to make a suggestion.
+* my e-mail address: erendore38k@gmail.com
+
+## Screenshots
+![CSharp ](/Github/Assets/Images/Metot.png)
+
+#### TR
+      
+### TR 
+* Burada amac metotlar hakkinda bilgi vermektir.
+* Ornekler basit seviyeye gore ayarlanmistir.
+* Oneride bulunmak icin e posta adresimden ulasabilirsiniz.
+
+
+
