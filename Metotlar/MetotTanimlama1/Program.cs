86e522532b54ed262764324ad8f199a45e029e38
Eren <erendore38k@gmail.com>
2023-11-23 16:34:06 +0300

    Method definition was demonstrated with the 2nd variation.

---
+++ Metotlar/MetotTanimlama1/Program.cs
@@ -0,0 +1,34 @@
+﻿
+/*
+ *Metodun parametre kısmında tanımlanıyorsa parametre,
+ *metodun gövdesinde ya da class içerisinde tanımlanıyorsa değişken ya da field. 
+ *Metotlarda sınırsız sayıda parametre değeri girilebilir. 
+ * 
+ * 
+ */
+
+using System;
+
+namespace metotlar
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Metot2(19);
+            
+        }
+
+        public static void Metot2(int a ) 
+        {
+         Console.WriteLine("1923");
+        }
+
+        public void Metot3(int c,bool b)
+        {
+
+        }
+
+    }
+}
