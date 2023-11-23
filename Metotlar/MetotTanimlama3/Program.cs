bcf78bba7d970a6c534d7e0c584e31dac6b520cf
Eren <erendore38k@gmail.com>
2023-11-23 21:20:03 +0300

    Method definition was demonstrated with the 4th variation.

---
+++ Metotlar/MetotTanimlama3/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+ *Metot Tanımlama/Oluşturma Varyasyonları - Geriye Değer Döndüren Parametre Alan Metot Türü
+ *
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+using System;
+
+namespace metotlar
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(1);
+        }
+
+        public int Metot7(int x)
+        {
+            return DateTime.Now.Year >2000 ? 1 :0 ;
+        }
+    }
+}
