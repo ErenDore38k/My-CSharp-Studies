7332d144836a45375188638e242ab7a6c82590db
Eren <erendore38k@gmail.com>
2023-11-24 16:39:08 +0300

    Usage of the Defined Method was explained.

---
+++ Metotlar/TanimlanmisMetodunKullanimi/Program.cs
@@ -0,0 +1,49 @@
+﻿
+/*
+ *Tanımlanmış Metodun Kullanımı - Tanımlandığı Sınıf İçerisindeki Metotlar Tarafından Kullanımı
+ *Tetikleme = Çağırma = Kullanımı 
+ *Bir metot tanımlandığı sınıf içerisindeki farklı bir metot içerisinden çağrılacaksa eğer tek yapılması gereken
+ *sadece isminin çağrılmasıdır.
+ *Bir class içerisinde oluşturduğumuz metotların diğer class’lar içerisinde erişilebilmesini işleyebilmemiz,
+ *anlayabilmemiz için class, nesne ve referans ilişkilerini hasbelkader bilmemiz gerekmektedir.
+
+ */
+
+using System;
+
+namespace functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            X(16);
+
+        }
+
+        static void X(int e )
+        {
+            Console.WriteLine(e);
+        }
+
+    }
+}
+
+/*class Örnek
+{
+    public void A()
+    {
+
+    }
+    private void B()
+    {
+        C(5);
+    }
+
+    private int C(int a )
+    {
+        return a;
+    }
+
+}
+*/
