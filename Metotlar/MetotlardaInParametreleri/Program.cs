8fc12ac23de7d8c9427688dbffc86c3ae629388b
Eren <erendore38k@gmail.com>
2023-11-25 14:16:06 +0300

    In Parameters topic has been added to Methods.

---
+++ Metotlar/MetotlardaInParametreleri/Program.cs
@@ -0,0 +1,43 @@
+﻿
+
+/*
+ *Metotlarda In Parametreleri (C# - In Keywordü)
+ *Parametrelere Dair Not:
+ 1)Parametre'nin değerini metodun içerisinde herhangi bir yerde çağırıp kullanabiliriz.
+ 2)Metot içerisinde üretilen herhangi bir değeri tutacak değişken oluşturmaktansa parametre üzerinde bu değeri tutabiliriz.
+Yani parametrenin değerini degiştirebiliriz.Çünkü parametreler özünde bir değişkendir.
+
+
+ *(C# - In Keywordü)
+ *In komutu sayesinde parametreye verilen değeri sabit tutabilmekteyiz.
+ *In komutu,metodun parametresini readonly(sadece okunabilir) hale getirir.
+ *In komutunun kullanıldığı parametrelerde eğer ki metot içerisinde farklı bir assing durumu söz konusu olursa derleyici
+ *hatası verecektir.
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+using System;
+
+namespace functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 2;
+            char c = 'q';
+
+            X(a, b, c); //X fonksiyonunu çağırma
+        }
+
+        static void X(in int a,int b ,in char c) 
+        {
+            Console.WriteLine($"a: {a}, b: {b}, c: {c}");
+        }
+    }
+}
