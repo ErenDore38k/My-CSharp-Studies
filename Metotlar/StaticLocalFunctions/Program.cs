95e2856bb946fe20884b5bdfe8e5ef13c98c1af5
Eren <erendore38k@gmail.com>
2023-11-26 20:17:36 +0300

    Static Local Functions topic added.

---
+++ Metotlar/StaticLocalFunctions/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+/*
+ *Static Local Functions(Metot İçerisinde Tanımlanabilir Statik Yerel Metotlar) 
+ *Static Local Functions kullanmak Local Functions kullanmaya nazaran daha performanslıdır.
+ */
+
+using System;
+
+namespace functions
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.X(45);
+        }
+
+       public void X(int a)
+        {
+            int b = 0;
+           static void Y(int a,int b)
+            {
+                System.Console.WriteLine(a);
+                System.Console.WriteLine(b);
+            }
+            Y(a,b);
+        }
+    }
+}
+
+
+
+
