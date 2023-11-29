673da8dfa58d53699e74cf1ed1455a2808a58aac
Eren <erendore38k@gmail.com>
2023-11-29 13:17:44 +0300

    Ref Returns topic has been added.

---
+++ Metotlar/RefReturnsOzelligi/Program.cs
@@ -0,0 +1,18 @@
+﻿
+/*
+ *C#'ta Ref Returns Özelliği Nedir? Ne Amaçla Kullanılmaktadır?
+ *Ref Returns özelliği sadece metotlarda geçerlidir.Çünkü returns(geri döndürme) yaptığımızdan dolayı returns da metotlarda yapılır.
+ *Metotlar geriye değer döndürebilen yapılardır.Ayrıca metotlarda geriye nesnelerde döndürebilmekteyiz.Ayrıca ref returns özelliği
+ *sayesinde değer türlü değişkenlerin referanslarınıda geriye döndürebilmekteyiz.
+ * 
+ */
+
+int a = 5;
+int b =  X( ref a);
+Console.WriteLine(a);
+Console.WriteLine(b);
+ ref int X( ref int y)
+{
+    y = 25;
+    return ref y;
+}
