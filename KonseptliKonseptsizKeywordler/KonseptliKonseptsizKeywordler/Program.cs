c840f00698dc155eef13c457fafd4f5b4a25c1d8
Eren <erendore38k@gmail.com>
2023-12-25 13:58:02 +0300

    Comment Lines Deleted

--- KonseptliKonseptsizKeywordler/KonseptliKonseptsizKeywordler/Program.cs
+++ KonseptliKonseptsizKeywordler/KonseptliKonseptsizKeywordler/Program.cs
@@ -1,31 +1,6 @@
-﻿// Keyword Nedir? - Operatörden Farkı Nedir?
-
-/*Keyword:Programlama dilinin en atomik parçaları diyebiliriz.
- *Derleyici için ön tanımlı olan,nerede hangi amaca hizmet edeceği belli ve kod içerisinde hangi noktalarda çağrılabileceği/ kullanılabileceği
- *kurallarla sınırlandırılmış olan anahtar sözcüklerdir.
- *Keywordlerin operatörlerden en farkı var? Cevap aşağıdaki satırda..
- *Operatörler esas olarak belirli bir operasyonu/eylemi üstlenen yapılardır.
- *Keywordler daha geniş kavramlardır. Bazen bir keyword bir operatörü temsil edebilir.
- *Operatörler esasında bir keyworddür lakin her keyword bir operatör değildir.
- */
-
-
-//Konseptli Keywordler Genel Bakış
-
-/*Konseptli Keywordler yapısal olarak tek başına kullanılmayan , bir bütün olarak bir konsepte bağlı olarak kullanılan yapılardır. 
- *Döngü yapıları , akış kontrol mekanizmaları vb yapılar. 
- */
-
+﻿
 for (int i = 0; i <= 10; i++)
 {
     Console.WriteLine(i);
 }
 
-//Yukarıdaki örnek konseptli keyword örneğidir.
-
-
-
-//Konseptsiz Keywordler Genel Bakış
-/*Herhangi bir bütüne, konsepte ihtiyaç duymaksızın anlam ifade eden keywordlere konseptsiz keyword denmektedir.
- *Örn break , continue , goto return.. gibi 
- */
