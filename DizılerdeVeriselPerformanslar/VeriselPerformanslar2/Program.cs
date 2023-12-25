a8557a8709056b486f0601a77055e796dacd6895
Eren <erendore38k@gmail.com>
2023-12-25 15:33:51 +0300

    Comment Lines Deleted

--- DizılerdeVeriselPerformanslar/VeriselPerformanslar2/Program.cs
+++ DizılerdeVeriselPerformanslar/VeriselPerformanslar2/Program.cs
@@ -1,48 +1,4 @@
-﻿//Dizilerde Verisel Açıdan Maliyetleri İnceleyelim
-
-/*
-int[] sayılar = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
-int[] sayılar2 = sayılar[2..7];
-sayılar2[0] *= 10;
-//SAyılar2 ayrı bir dizi olduğu için sayılar2'nin 0.imdeksinde yapacağım işlem sayılar2 dizisine etki edecek sayılar dizisinde
-//etki etmemiş olacak. 
-*/
-
-
-
-
-
-//ArraySegment Türü Nedir?
-//Bir dizinin bütününden ziyade belirli bir kısmına yahut parçasına ihtiyaç dahilinde ilgili diziyi kopyalamak yerine (ki görece
-//oldukça maliyetli bir operasyondur) bağımsız bir referans ile erişmemizi ve böylece salt bir şekilde temsil etmemizi sağlayan
-//bir yapıdır.
-
-
-
-
-//ArraySegment Türü İle Dizinin Belli Bir Alanını Referans Etmek
-
-/*
-
-int[] sayılar = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
-
-ArraySegment<int> segment1 = new ArraySegment<int>(sayılar); // Dizinin tüm elemanlarını temsil eder.
-
-ArraySegment<int> segment2 = new ArraySegment<int>(sayılar,2,5); // Bu ise 30,40,50,60,70 değerlerini tutuyor.
-segment1 [0] *= 10;
-segment2[0] *= 10;
-
-*/
-
-
-
-
-//ArraySegment Slicing(Dilimleme) Özelliği
-
-//Bir dizi üzerinde birden fazla parçada çalışacaksan eğer herbir parçayı ayrı bir ArraySegment olarak tanımlayabiliriz.
-//Bu tanımlamaların dışında diziyi tek bir ArraySegment ile referans edip ilgili parçaları o segment üzerinden talep edebiliriz.
-//Yani ilgili diziyi tek bir segment üzerinden daha rahat bir şekilde parçalayabiliriz. Bu durumda bize yazılımsal açıdan
-//efektiflik kazandırmış olacaktır.
+﻿
 
 int[] sayilar = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
 
